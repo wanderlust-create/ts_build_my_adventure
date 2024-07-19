38e50882314cbb0f8c48b09a0e95937e691b1c3c
Tamara Dowis <tamara.dowis@gmail.com>
2024-07-19 15:59:27 -0500

    add port access .env or cli

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,21 +1,37 @@
+import * as dotenv from "dotenv";
 import { isInteger } from "./utils";
 
+// Set the default NODE_ENV to 'development'
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-const findPORT = function() {
-  let port: number;
-  const portArg = process.env.PORT;
-  if (isInteger(portArg)) {
-    port = parseInt(portArg)
-  }
-  if(!port) {
-    port = 8080
-  }
-  return port
+// load .env File
+const envFound = dotenv.config();
+if (!envFound) {
+  throw new Error("⚠️Could not find .env file⚠️");
 }
+
+const findPORT = function () {
+  let port: number;
+  const portEnv = process.env.PORT,
+    portArg = process.argv[2]
+    // first look for port num in .env file
+  if (isInteger(portEnv)) {
+    port = parseInt(portEnv);
+  }
+  // then look for a comand line arg
+  if (!port && isInteger(portArg)) {
+    port = parseInt(portArg);
+  }
+  if (!port) {
+    port = 8080;
+  }
+  return port;
+};
 
 // Define port number
 const port = findPORT();
 
 export default {
-  PORT: port
-}
+  PORT: port,
+  NODE_ENV: process.env.NODE_ENV,
+};
