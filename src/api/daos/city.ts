3585177b17ac037fff33e50afd016c6c8339eaf1
Tamara Dowis <tamara.dowis@gmail.com>
2024-07-24 13:33:39 -0500

    city DAO

---
+++ src/api/daos/city.ts
@@ -0,0 +1,17 @@
+import logger from "../../loaders/logger";
+import City from "../models/city";
+
+export default {
+  listAllCities,
+  // getCityById,
+  // createCity,
+  // updateCityById,
+  // deleteCityById,
+};
+
+async function listAllCities(): Promise<City[]> {
+  logger.debug(`Entering GET DAO- cities/ endpoint.`);
+  return City.query()
+    .column("id", "name", "country")
+    .orderBy("created_at", "desc")
+}
