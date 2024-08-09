5a7efcb7b66c425702bc67457eafca5f1965cc4b
Tamara Dowis <tamara.dowis@gmail.com>
2024-08-09 18:35:25 -0500

    move req body validation req to middlewear

--- src/api/middlewear/validate-dto.ts
+++ src/api/middlewear/validate-dto.ts
@@ -1,10 +1,10 @@
 import { Request, Response, NextFunction } from "express";
 import * as yup from "yup";
 // Load Logger
-import logger from "../../../loaders/logger";
+import logger from "../../loaders/logger";
 
 // Error handling function
-import ApiError from "../error/apiError";
+import ApiError from "../reqBodyValidation/error/apiError";
 
 export default function validateDto(schema: { validate: (arg0: any) => any; }) {
   return async (req: Request, res: Response, next: NextFunction) => {
