b835b587f85b25680240002222a3e139747aecde
Tamara Dowis <tamara.dowis@gmail.com>
2024-08-10 16:00:24 -0500

    add reqBody validation to city post req

--- src/api/routes/api/city.ts
+++ src/api/routes/api/city.ts
@@ -1,13 +1,17 @@
 import { NextFunction, Request, Response, Router } from "express";
 import CityController from "../../controllers/city";
 
+// Error Handler
+import validateDto from "../../middlewear/validate-dto";
+import cityDto from "../../reqBodyValidation/dtos/city";
+
 const route = Router();
 
 export default (app: Router) => {
   app.use("/cities", route);
   route.get("/", CityController.listAllCities);
   route.get("/:id", CityController.getCityById);
-  route.post("/", CityController.createCity);
+  route.post("/", validateDto(cityDto.data), CityController.createCity);
   route.patch("/:id", CityController.updateCityById);
   route.delete("/:id", CityController.deleteCityById);
 };
