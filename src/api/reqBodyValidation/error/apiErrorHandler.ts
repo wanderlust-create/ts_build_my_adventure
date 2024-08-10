3846d2c09e5f7141543c5ab1141033aeb02dfdf8
Tamara Dowis <tamara.dowis@gmail.com>
2024-08-10 15:51:27 -0500

    update err type and add logging debug statements

--- src/api/reqBodyValidation/error/apiErrorHandler.ts
+++ src/api/reqBodyValidation/error/apiErrorHandler.ts
@@ -3,20 +3,23 @@ import logger from "../../../loaders/logger";
 import { Request, Response, NextFunction } from "express";
 
 export default function apiErrorHandler(
-  err: any,
+  err: { code: number; message: any; },
   req: Request,
   res: Response,
   next: NextFunction
 ) {
   try {
+    logger.debug("ENTERING ApiErrorHandler try");
     logger.error(err);
   } catch (error) {
+    logger.debug("ENTERING ApiErrorHandler catch");
     console.error("Error occurred during logging:", error);
   }
 
   if (err instanceof ApiError) {
+    logger.debug("ENTERING apiErrorHandler");
     return res.status(err.code).json(err.message);
   }
 
-  return res.status(500).json("An error occurred");
-}
+  return res.status(500).json("Internal Server Error");
+  }
