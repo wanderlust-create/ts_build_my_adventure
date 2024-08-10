61567ac49b8f4a817cff05aba57bdec84f5c10e1
Tamara Dowis <tamara.dowis@gmail.com>
2024-08-10 15:48:42 -0500

    refactored validateDto to pass through all errors

--- src/api/middlewear/validate-dto.ts
+++ src/api/middlewear/validate-dto.ts
@@ -1,21 +1,30 @@
 import { Request, Response, NextFunction } from "express";
-import * as yup from "yup";
+import { AnyObjectSchema } from "yup";
+
 // Load Logger
 import logger from "../../loaders/logger";
 
 // Error handling function
 import ApiError from "../reqBodyValidation/error/apiError";
 
-export default function validateDto(schema: { validate: (arg0: any) => any; }) {
+export default function validateDto(schema: AnyObjectSchema) {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const validatedBodyData = await schema.validate(req.body);
-      // replace req.body with validated values
+      logger.debug(
+        `req body validation: Entering validateDto: req body is: ${req.body}`
+      );
+      const validatedBodyData = await schema.validate(req.body, {
+        // returns all errors
+        abortEarly: false,
+      });
+      // replace req.body with validated dto
       req.body = validatedBodyData;
-      logger.debug(`AFTER VALIDATION :req body is: ${req.body}`);
+      logger.debug(
+        `Req body Validation: Post Schema validation: req body is: ${req.body}`
+      );
       next();
-    } catch (err) {
-      next(ApiError.badRequest(err));
+    } catch (e) {
+      next(ApiError.badRequest(e.errors));
     }
   };
 }
