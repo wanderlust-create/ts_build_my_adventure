5560d4747746866153a96b91753a3b5af746425d
Tamara Dowis <tamara.dowis@gmail.com>
2024-08-10 15:50:45 -0500

    add logging debug statements

--- src/api/reqBodyValidation/error/apiError.ts
+++ src/api/reqBodyValidation/error/apiError.ts
@@ -1,12 +1,15 @@
+import logger from "../../../loaders/logger"
+
 export default class ApiError {
-  message: string;
   code: number;
+  message: string;
   constructor(code: number, message: string) {
+    logger.debug(`ENTERING ApiError- top: message ${message} code: ${code}`)
     this.message = message;
     this.code = code;
   }
-
   static badRequest(msg: string) {
+    logger.debug(`ENTERING ApiError- bad req ${msg}`)
     return new ApiError(400, msg);
   }
 }
