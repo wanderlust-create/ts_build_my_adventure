15a03bb6a8f387cc1f4ff475d38fb0526635de76
Tamara Dowis <tamara.dowis@gmail.com>
2024-08-11 20:12:16 -0500

    update error in min char for cityId

--- src/api/reqBodyValidation/dtos/event.ts
+++ src/api/reqBodyValidation/dtos/event.ts
@@ -2,7 +2,7 @@ import { object, string } from "yup";
 
 const eventPostDto = {
   data: object().shape({
-    cityId: string().min(3).max(30).required(),
+    cityId: string().min(1).max(30).required(),
     title: string().min(3).max(30).required(),
   }),
 };
@@ -10,7 +10,7 @@ const eventPostDto = {
 //FYI will convert req.body numbers to string
 const eventPatchDto = {
   data: object().shape({
-    cityId: string().min(3).max(30),
+    cityId: string().min(1).max(30),
     title: string().min(3).max(30),
   }),
 };
